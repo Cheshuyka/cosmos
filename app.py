17c09dbe824e5d09bdbd4b4cd89421510e0f4d81
Cheshuyka <cheshuyka.ref@gmail.com>
2023-11-04 23:42:27 +0300

    Сделал заготовку для видеоплеера

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('base.html')
 
 
+@app.route('/video')  # страница с видео
+def video():
+    return render_template('video.html')
+
+
 if __name__ == "__app__":
     app.run(debug=True)
