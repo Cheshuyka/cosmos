010640dff0294aadb52652b61a4776264ae6a2ec
Cheshuyka <cheshuyka.ref@gmail.com>
2023-12-26 01:00:29 +0300

    HELP

--- app.py
+++ app.py
@@ -19,5 +19,25 @@ def video():
     return render_template('video.html')
 
 
+@app.route('/video1')  # страница 2 с видео
+def video1():
+    return render_template('video1.html')
+
+
+@app.route('/video2')  # страница 2 с видео
+def video2():
+    return render_template('video2.html')
+
+
+@app.route('/video3')  # страница 2 с видео
+def video3():
+    return render_template('video3.html')
+
+
+@app.route('/video4')  # страница 2 с видео
+def video4():
+    return render_template('video4.html')
+
+
 if __name__ == "__app__":
     app.run(debug=True)
